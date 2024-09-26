022ccb1de66433d7164c84b1c75e2b69e8c10d86
bestisblessed <tyler.durette@gmail.com>
2024-09-26 19:44:24 -0400

    im dumb

--- DASHBOARD/run.sh
+++ DASHBOARD/run.sh
@@ -3,3 +3,7 @@
 scp -r Neo:~/odds-monitoring/data .
 
 python monitor_odds_movement.py
+
+echo "1. Now, manually run 'app.py' by executing: python app.py"
+echo "2. After that, manually run 'dashboard.py' by executing: python dashboard.py"
+echo "Once both scripts are running, you can access the app at http://127.0.0.1:8050"
