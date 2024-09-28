e1e4179204bf4f611432126a259fcc1e03f26c19
bestisblessed <tyler.durette@gmail.com>
2024-09-28 18:46:32 -0400

    im dumb

--- ncaaf.py
+++ ncaaf.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service  # Import Service
 from selenium.webdriver.support import expected_conditions as EC
 import json
 import datetime
