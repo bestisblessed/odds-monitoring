3989d5de07cf3239a35078239470d35581c63c34
Tyler <td@mac.attlocal.net>
2024-12-19 11:41:52 -0500

    im dumb

--- zip.sh
+++ zip.sh
@@ -3,13 +3,14 @@
 current_date=$(date +%b_%d_%y | tr '[:lower:]' '[:upper:]')
 
 # Create zip file for all data
-zip -r zips/data_backup_${current_date}.zip data
-echo "Backup created: zips/data_backup_${current_date}.zip"
+mkdir -p zips
+# zip -r zips/data_backup_${current_date}.zip data
+# echo "Backup created: zips/data_backup_${current_date}.zip"
 
 # Create zip files for each sport based on file naming convention
-zip -r zips/ufc_backup_${current_date}.zip data/ufc_odds_vsin_*.json
-zip -r zips/ncaaf_backup_${current_date}.zip data/ncaaf_odds_vsin_*.json
-zip -r zips/nfl_backup_${current_date}.zip data/nfl_odds_vsin_*.json
+zip -r zips/nfl_backup_${current_date}.zip NFL/Scraping/data/odds/nfl_odds_vsin_*.json
+zip -r zips/ufc_backup_${current_date}.zip UFC/Scraping/data/ufc_odds_vsin_*.json
+zip -r zips/ncaaf_backup_${current_date}.zip NCAAF/Scraping/data/ncaaf_odds_vsin_*.json
+echo "Backup created: zips/nfl_backup_${current_date}.zip"
 echo "Backup created: zips/ufc_backup_${current_date}.zip"
 echo "Backup created: zips/ncaaf_backup_${current_date}.zip"
-echo "Backup created: zips/nfl_backup_${current_date}.zip"
