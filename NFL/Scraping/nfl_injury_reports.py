e1b457859524e5ba681ce393a8286b814651df16
bestisblessed <tyler.durette@gmail.com>
2025-01-24 03:44:54 -0500

    fixing chromium quitting in injury reports

--- NFL/Scraping/nfl_injury_reports.py
+++ NFL/Scraping/nfl_injury_reports.py
@@ -17,25 +17,29 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")  
 service = Service(chromedriver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
-url = 'https://www.espn.com/nfl/injuries'
-driver.get(url)
-wait = WebDriverWait(driver, 10)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, "ResponsiveTable")))
-tables = driver.find_elements(By.CLASS_NAME, "ResponsiveTable")
-all_data = []
-for table in tables:
-    headers = [th.text for th in table.find_elements(By.TAG_NAME, "th")]
-    rows = table.find_elements(By.TAG_NAME, "tr")
-    for row in rows:
-        row_data = [td.text for td in row.find_elements(By.TAG_NAME, "td")]
-        if row_data:
-            all_data.append(row_data)
-df = pd.DataFrame(all_data, columns=headers)
-script_dir = os.path.dirname(os.path.abspath(__file__))
-output_dir = os.path.join(script_dir, "data/injury-reports")
-os.makedirs(output_dir, exist_ok=True)
-today_date = datetime.now().strftime("%Y-%m-%d")
-csv_file_path = os.path.join(output_dir, f"nfl_injury_status_{today_date}.csv")
-df.to_csv(csv_file_path, index=False)
-driver.quit()
-print(f"Data saved to {csv_file_path}")
+
+try:
+    url = 'https://www.espn.com/nfl/injuries'
+    driver.get(url)
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "ResponsiveTable")))
+    tables = driver.find_elements(By.CLASS_NAME, "ResponsiveTable")
+    all_data = []
+    for table in tables:
+        headers = [th.text for th in table.find_elements(By.TAG_NAME, "th")]
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        for row in rows:
+            row_data = [td.text for td in row.find_elements(By.TAG_NAME, "td")]
+            if row_data:
+                all_data.append(row_data)
+    df = pd.DataFrame(all_data, columns=headers)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    output_dir = os.path.join(script_dir, "data/injury-reports")
+    os.makedirs(output_dir, exist_ok=True)
+    today_date = datetime.now().strftime("%Y-%m-%d")
+    csv_file_path = os.path.join(output_dir, f"nfl_injury_status_{today_date}.csv")
+    df.to_csv(csv_file_path, index=False)
+    print(f"Data saved to {csv_file_path}")
+
+finally:
+    driver.quit()
