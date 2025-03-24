521341b8a5c849cf1e8214002131b4abf4d77248
Trinity <tyler.durette@gmail.com>
2025-03-24 06:42:10 -0400

    im dumb

--- UFC/Scraping/fightoddsio.py
+++ UFC/Scraping/fightoddsio.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
@@ -12,9 +13,13 @@ import subprocess
 import pandas as pd
 import re
 
+# Find chromedriver path using 'which'
+chromedriver_path = "/usr/bin/chromedriver"
+
+# Configure Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-dev-shm-usage")  # Required for running on Raspberry Pi
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--user-data-dir=/tmp/chrome-temp")
 chrome_options.add_experimental_option("prefs", {
@@ -23,7 +28,9 @@ chrome_options.add_experimental_option("prefs", {
     "download.prompt_for_download": False
 })
 
-driver = webdriver.Chrome(options=chrome_options)
+# Initialize the Chrome WebDriver with the options and service
+service = Service(chromedriver_path)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get("https://fightodds.io/")
 driver.implicitly_wait(5)
 
