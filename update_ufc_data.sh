3d3bc8e5cf57574f4d65dc9d9a95c7558c359d51
bestisblessed <tyler.durette@gmail.com>
2025-08-08 15:36:59 -0400

    im dumb

--- update_ufc_data.sh
+++ update_ufc_data.sh
@@ -15,7 +15,7 @@ mkdir -p UFC/Scraping/data
 unzip -o zips/ufc_backup_${current_date}.zip "UFC/Scraping/data/*" -d ./
 echo "Restored: UFC/Scraping/data/"
 
-### PROCESS ODDS DATA AND COPY TO STREAMLIT $ SWIFT APP
+### PROCESS ODDS DATA AND COPY TO SWIFT APP & STREAMLIT
 cd ~/Code/odds-monitoring/UFC/Analysis
 python ufc_odds_data_processing_fightoddsio.py
 cp /Users/td/Code/odds-monitoring/UFC/Analysis/data/ufc_odds_movements_fightoddsio.csv ~/Code/mma-ai-swift-app/data/
@@ -24,7 +24,11 @@ scp /Users/td/Code/odds-monitoring/UFC/Analysis/data/ufc_odds_movements_fightodd
 #git add data/ufc_odds_movements_fightoddsio.csv -f
 #git commit -m "update odds data"
 #git push
-
+cp /Users/td/Code/odds-monitoring/UFC/Analysis/data/ufc_odds_movements_fightoddsio.csv /Users/td/Code/mma-ai/Streamlit/data/
+cd /Users/td/Code/mma-ai/Streamlit
+git add data/ufc_odds_movements_fightoddsio.csv -f
+git commit -m "update odds data"
+git push
 
 # DELETE REMOTE UFC ODDS DATA FILES
 #ssh Trinity "rm -rf /home/trinity/odds-monitoring/UFC/Scraping/data/* && echo 'Cleared UFC data files on Trinity'"
