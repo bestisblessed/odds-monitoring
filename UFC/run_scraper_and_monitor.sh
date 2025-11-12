8260267efc950f96cb24d7d60e41b88cecd86499
Trinity <tyler.durette@gmail.com>
2025-11-12 03:40:30 -0500

    im dumb

--- UFC/run_scraper_and_monitor.sh
+++ UFC/run_scraper_and_monitor.sh
@@ -6,6 +6,6 @@ date
 sleep 5
 /home/trinity/.pyenv/shims/python /home/trinity/odds-monitoring/UFC/Scraping/ufc_totals.py >> /home/trinity/odds-monitoring/UFC/Scraping/log_totals.log 2>&1
 sleep 5
-/home/trinity/.pyenv/shims/python /home/trinity/odds-monitoring/UFC/Monitoring/ufc_monitor_odds_movement.py >> /home/trinity/odds-monitoring/UFC/Monitoring/ufc_monitor.log 2>&1i
+/home/trinity/.pyenv/shims/python /home/trinity/odds-monitoring/UFC/Monitoring/ufc_monitor_odds_movement.py >> /home/trinity/odds-monitoring/UFC/Monitoring/ufc_monitor.log 2>&1
 
 echo ""
